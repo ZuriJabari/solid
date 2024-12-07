20386250c511b140b649a1722c25780ca9b66923
ZuriJabari <zurizabari@icloud.com>
2024-12-07 11:33:53 +0300

    test(backend): Complete test suite execution with all 49 tests passing across all apps

--- mobile/UrbanHerb/jest.config.js
+++ mobile/UrbanHerb/jest.config.js
@@ -1,1 +1,14 @@
-module.exports = { preset: "jest-expo", transformIgnorePatterns: ["node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"], setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"], moduleFileExtensions: ["ts", "tsx", "js", "jsx"], moduleDirectories: ["node_modules", "src"] };
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+  ],
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js'
+  },
+  testEnvironment: 'node',
+};
